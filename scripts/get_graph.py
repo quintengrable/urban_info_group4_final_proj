7252513a1bf86c5379c49d703b09b8ba574a2fd9
cindy471 <li_cindy@berkeley.edu>
2026-04-10 15:43:16 -0700

    nothing works

--- scripts/get_graph.py
+++ scripts/get_graph.py
@@ -9,11 +9,8 @@ from helper_function import get_path
 bay_area_counties = ['Alameda', 'Contra Costa', 'Marin', 'Napa', 'San Francisco', 'San Mateo', 'Santa Clara', 'Solano', 'Sonoma']
 graphs = {}
 
-for county in bay_area_counties: 
-    pass_name = county + ' County, California, USA'
-    print(f"Downloading: {pass_name}")
-    graphs[county] = ox.graph_from_place((pass_name), network_type = "drive")
+print('downloading')
+G = ox.graph_from_place(bay_area_counties, network_type="drive", which_result=None)
 
-G = nx.compose_all(list(graphs.values()))
 ox.save_graphml(G, get_path("processed", "G.graphml"))
 print(f'Saved to: {get_path("processed", "G.graphml")}')
