927cf1d1a6c8fec31611b1fc5ad542028f4c6197
Quinten Grable <quintengrable@berkeley.edu>
2026-04-12 12:42:33 -0700

    implement bay area network on top of example code

--- scripts/pub_trans_mobility.py
+++ scripts/pub_trans_mobility.py
@@ -1,12 +1,64 @@
 # %%
-import geopandas
+import geopandas as gpd
 import r5py
 import r5py.sampledata.helsinki
 import shapely
+import datetime as dt
+from pathlib import Path
 
-population_grid = geopandas.read_file(r5py.sampledata.helsinki.population_grid)
+#%%
+#this will pull the data from the correct folder regardless of where cwd is
+base_path = Path(__file__).parent.parent
+data_file = base_path / "data" / "processed" / "day_night_pop_change.parquet"
 
-railway_station = geopandas.GeoDataFrame(
+#%%
+#pull in data from previous analyses
+population_grid1 = gpd.read_parquet(data_file)
+population_grid1.head()
+
+#%%
+#clean dataset to only relevant columns
+population_grid1 = population_grid1.drop(columns=['FUNCSTAT','INTPTLAT','INTPTLON','geoid','work_pop','home_pop','within_tract','resident_pop','daytime_pop'])
+population_grid1.head()
+
+#%%
+#get centroids
+population_grid1 = population_grid1.to_crs(epsg=2227)
+population_grid1['centroids'] = population_grid1.geometry.centroid
+population_grid1 = population_grid1.set_geometry('geometry')
+
+population_grid1 = population_grid1.to_crs(epsg=4326)
+
+#%%
+#testing with a landmark
+ferry_building = gpd.GeoDataFrame(
+    {
+        "id": ["ferry_building"],
+        "geometry": [shapely.Point(-122.3937, 37.7955)]
+    },
+    crs="EPSG:4326",
+)
+
+overview_map1 = population_grid1.explore("total_pop", cmap="Reds")
+overview_map1 = ferry_building.explore(m=overview_map1, marker_type="marker")
+overview_map1
+
+#%%
+#creating a bay area transport network
+bay_area_pbf = base_path / "data" / "raw" / "san-francisco-bay_california.osm.pbf"
+bay_area_gtfs = base_path / "data" / "raw" / "GTFSTransitData_RG.zip"
+
+transport_network = r5py.TransportNetwork(
+    bay_area_pbf,
+    [bay_area_gtfs]
+)
+
+transport_network
+#%%
+#example code
+population_grid = gpd.read_file(r5py.sampledata.helsinki.population_grid)
+
+railway_station = gpd.GeoDataFrame(
     {
         "id": ["railway_station"],
         "geometry": [shapely.Point(24.94152, 60.17066)]
@@ -32,9 +84,8 @@ transport_network
 
 # %%
 print(transport_network)
-# %%
-import datetime
 
+# %%
 origins = population_grid.copy()
 origins.geometry = origins.geometry.centroid
 
@@ -44,7 +95,7 @@ travel_times = r5py.TravelTimeMatrix(
     transport_network,
     origins=origins,
     destinations=destinations,
-    departure=datetime.datetime(2022, 2, 22, 8, 30),
+    departure=dt.datetime(2022, 2, 22, 8, 30),
     transport_modes=[
         r5py.TransportMode.TRANSIT,
         r5py.TransportMode.WALK,
