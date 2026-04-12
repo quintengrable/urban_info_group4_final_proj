00b6d27058e3b2f91adac74226b332d8687803d0
Quinten Grable <quintengrable@berkeley.edu>
2026-04-12 13:48:35 -0700

    Update pub_trans_mobility.py

--- scripts/pub_trans_mobility.py
+++ scripts/pub_trans_mobility.py
@@ -13,21 +13,21 @@ data_file = base_path / "data" / "processed" / "day_night_pop_change.parquet"
 
 #%%
 #pull in data from previous analyses
-population_grid1 = gpd.read_parquet(data_file)
-population_grid1.head()
+population_grid = gpd.read_parquet(data_file)
+population_grid.head()
 
 #%%
 #clean dataset to only relevant columns
-population_grid1 = population_grid1.drop(columns=['FUNCSTAT','INTPTLAT','INTPTLON','geoid','work_pop','home_pop','within_tract','resident_pop','daytime_pop'])
-population_grid1.head()
+population_grid = population_grid.drop(columns=['FUNCSTAT','INTPTLAT','INTPTLON','geoid','work_pop','home_pop','within_tract','resident_pop','daytime_pop'])
+population_grid.head()
 
 #%%
 #get centroids
-population_grid1 = population_grid1.to_crs(epsg=2227)
-population_grid1['centroids'] = population_grid1.geometry.centroid
-population_grid1 = population_grid1.set_geometry('geometry')
+population_grid = population_grid.to_crs(epsg=2227)
+population_grid['centroids'] = population_grid.geometry.centroid
+population_grid = population_grid.set_geometry('geometry')
 
-population_grid1 = population_grid1.to_crs(epsg=4326)
+population_grid = population_grid.to_crs(epsg=4326)
 
 #%%
 #testing with a landmark
@@ -39,9 +39,9 @@ ferry_building = gpd.GeoDataFrame(
     crs="EPSG:4326",
 )
 
-overview_map1 = population_grid1.explore("total_pop", cmap="Reds")
-overview_map1 = ferry_building.explore(m=overview_map1, marker_type="marker")
-overview_map1
+overview_map = population_grid.explore("total_pop", cmap="Reds")
+overview_map = ferry_building.explore(m=overview_map, marker_type="marker")
+overview_map
 
 #%%
 #creating a bay area transport network
@@ -58,19 +58,19 @@ transport_network
 
 #%%
 #bay area origins
-origins1 = population_grid1.head(20).copy()
-origins1['id'] = origins1['GEOID']
-origins1 = origins1.set_geometry('centroids')
-origins1.head(3)
+origins = population_grid.head(20).copy()
+origins['id'] = origins['GEOID']
+origins = origins.set_geometry('centroids')
+origins.head(3)
 
 #%%
-#bay area test destination
-destinations1 = ferry_building.copy()
+#bay area test destination create tt matrix
+destinations = ferry_building.copy()
 
 travel_times = r5py.TravelTimeMatrix(
     transport_network,
-    origins=origins1,
-    destinations=destinations1,
+    origins=origins,
+    destinations=destinations,
     departure=dt.datetime(2026, 4, 14, 8, 30),
     transport_modes=[
         r5py.TransportMode.TRANSIT,
@@ -83,3 +83,26 @@ travel_times = r5py.TravelTimeMatrix(
 travel_times.head(30)
 
 #%%
+#create a map of the travel times
+travel_times_mapping = population_grid.merge(travel_times, left_on="GEOID", right_on="from_id")
+travel_times_mapping.head()
+
+#%%
+#save tt matrix to processed data folder
+pub_trans_tt_path = base_path / "data" / "processed" / "pub_trans_tt_mapping.csv"
+travel_times_mapping.to_csv(pub_trans_tt_path)
+#%%
+#read in tt matrix so we don't have to run all the above calulcations again
+pub_trans_tt_path = base_path / "data" / "processed" / "pub_trans_tt_mapping.csv"
+travel_times_mapping1 = gpd.read_file(pub_trans_tt_path)
+
+#%%
+travel_times_mapping1.head()
+
+#%%
+travel_times_mapping.head()
+#%%
+#travel_times = travel_times.set_geometry('geometry') 
+
+#%%
+travel_times_mapping.explore("travel_time", cmap="Greens")
