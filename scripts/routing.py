188d492451af34e95326cf8f30e137f4d9ce2303
cindy471 <li_cindy@berkeley.edu>
2026-04-13 21:05:49 -0700

    testing largest connected component

--- scripts/routing.py
+++ scripts/routing.py
@@ -4,14 +4,17 @@ import os
 from pathlib import Path
 from helper_function import get_path
 import osmnx as ox
-# folder = "processed"
-# filename = "test"
-# parent_path = Path.cwd().parent/ ("data/" + folder) # test 
-# print("Data directory:", parent_path.resolve())
-# file_path = os.path.join(parent_path, filename)
-# print(file_path)
-print(get_path("processed", "bay_area_lodes_od_table.parquet"))
-#bay_area_lodes = pd.read_parquet(get_path("processed", "bay_area_lodes_od_table.parquet"))
+
+bay_area_lodes = pd.read_parquet(get_path("processed", "bay_area_lodes_od_table.parquet"))
 #%%
 G_path = get_path('processed', 'G.graphml')
 G = ox.load_graphml(G_path)
+
+# %%
+G_connected = ox.utils_graph.get_largest_component(G, strongly=True)
+
+# %%
+ox.plot_graph(G_connected, node_size = 0)
+# %%
+ox.plot_graph(G, node_size = 0)
+# %%
