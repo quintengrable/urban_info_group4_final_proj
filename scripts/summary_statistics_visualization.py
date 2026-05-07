0e26b0632cf8855590a0d562d5ad136f8cf98408
cindy471 <li_cindy@berkeley.edu>
2026-05-07 11:32:30 -0700

    sort y axis by difference in day/night pop

--- scripts/summary_statistics_visualization.py
+++ scripts/summary_statistics_visualization.py
@@ -6,6 +6,7 @@ import numpy as np
 import sys
 import os
 from helper_function import get_path
+from pathlib import Path
 
 bay_area_lodes = pd.read_parquet(get_path("processed", "bay_area_lodes_od_table.parquet"))
 
@@ -46,8 +47,8 @@ bay_geocode_map = {'06001': 'Alameda',
 county_pop = county_pop[county_pop.index.isin(bay_area_geocodes)]
 
 county_pop['County'] = county_pop.index.map(bay_geocode_map)
-
-county_pop
+county_pop['pop_diff'] = county_pop['work_pop'] - county_pop['home_pop']
+county_pop = county_pop.sort_values(by='pop_diff', ascending=True)
 
 # plot day vs night populations per county
 fig, ax = plt.subplots(figsize=(9, 6))
@@ -57,8 +58,8 @@ y = county_pop['County']
 y_pos = np.arange(len(y))
 height = 0.35
 
-ax.barh(y_pos - height/2, width = county_pop['work_pop'], height = height, color = '#f39c12', label = "Work County")
-ax.barh(y_pos + height/2, width = county_pop['home_pop'], height = height, color = '#0000D1', label = "Home County")
+ax.barh(y_pos + height/2, width = county_pop['work_pop'], height = height, color = '#f39c12', label = "Work County")
+ax.barh(y_pos - height/2, width = county_pop['home_pop'], height = height, color = '#0000D1', label = "Home County")
 
 ax.set_yticks(y_pos)
 ax.set_yticklabels(y)
@@ -71,7 +72,16 @@ ax.set_title('County Daytime vs Nighttime Population')
 ax.spines[['top', 'right']].set_visible(False)
 
 ax.legend(loc='lower right')
-#plt.savefig("/content/drive/MyDrive/Urban_Informatics_Final_Project/Colab/Visuals/day_night_county.png")
+
+save_path = Path(__file__).parent.parent / "visualizations" / "day_night_county.png"
+plt.savefig(
+    save_path, 
+    dpi=300,              
+    bbox_inches='tight',   
+    facecolor='white',     
+    transparent=False
+)
 
 plt.tight_layout()
 plt.show()
+
